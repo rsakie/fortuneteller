3fd8a986ee4785c78ea0c0449e78748eeefe9067
rsakie <rsakie@yahoo.com>
2017-07-02 10:06:46 -0400

    fortuneteller tuneup

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -11,28 +11,30 @@ namespace FortuneTeller
         static void Main(string[] args)
         {
             // welcome header 
-            Console.WriteLine("Welcome to the Wonderful, Amazing, Extrordinary Bobo The Fortune Teller!! \n");
-            Console.WriteLine("I am going to predict your future. \n");
+            Console.WriteLine("Welcome to the Wonderful, Amazing, Extrordinary Bobo The Fortune Teller!!\n");
+            Console.WriteLine("I am going to predict your future.\n");
+           
             //have user enter info as promted f/l name,birth month
             //age as digit(int)
-            //enter fav roygbiv, if dont know ask for help
+            //enter fav roygbiv, if dont know ask for "help"
             //
             Console.WriteLine("Please enter your First Name :");
             string fName = Console.ReadLine();
 
             Console.WriteLine("Please enter your Last Name :");
             string lName = Console.ReadLine();
-
+           
             Console.WriteLine("Please enter your Age: ");
             int age = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Wow, You look good for your age!! \n");
-            Console.WriteLine("Please enter your Birth Month as a Digit");
+            Console.WriteLine("Please enter your Birth Month as a Digit:");
             int birthM = int.Parse(Console.ReadLine());
             //could not get code to error out or restart if birthM <1 or birthM>12
 
             Console.WriteLine("Please enter your Favorite ROYGBIV Color:  if you do not know what ROYGBIV is   enter \"Help\" ");
             string color = Console.ReadLine();
+
             if (color.ToLower() == "help")
             {
                 Console.WriteLine("The Colors are Red, Orange, Yellow, Green, Blue, Indigo, Violet\n");
@@ -41,7 +43,7 @@ namespace FortuneTeller
             }
             Console.WriteLine("How many Siblings do you have?");
 
-            int sibs = int.Parse(Console.ReadLine());
+            int sib = int.Parse(Console.ReadLine());
 
             //// attemted stretch goal
             ////if user want to quit, type quit
@@ -52,6 +54,7 @@ namespace FortuneTeller
             {
                 Console.Write(fName + " " + lName);
             }
+
             if (age % 2 == 0)
             {
                 Console.Write(" you will Retire in 35 years,\n");
@@ -62,7 +65,7 @@ namespace FortuneTeller
             }
             if (birthM >= 1 && birthM <= 4)
             {
-                Console.Write("You will be Blessed with a fortune of $42,000 in the bank upon retirement,\n");
+                Console.Write("You will be Blessed with a fortune of $42,000.00 in the bank upon retirement,\n");
             }
             else if (birthM >= 5 && birthM <= 8)
             {
@@ -76,30 +79,28 @@ namespace FortuneTeller
             {
                 Console.Write("You will be Poor with not even a penny in the bank,\n");
             }
-            //how many sibs 0-you will have a  vac home in (loc), 2 vac home loc, 3 ..., 3 or more...
-            if (sibs == 0)
+
+            int siblings = 3;
+            switch (siblings)
             {
-                Console.Write("You will have a Vacation Home in Argentina,\n");
-            }
-            else if (sibs == 1)
-            {
-                Console.Write("You will have a Vacation Home in Paris,\n");
-            }
-            else if (sibs == 2)
-            {
-                Console.Write("You will have a Vacation Home in Hawaii,\n");
-            }
-            else if (sibs == 3)
-            {
-                Console.Write("You will have a Vacation Home in Florida,\n");
-            }
-            else if (sibs > 3)
-            {
-                Console.Write("You will have a Vacation Home in Colorado,\n");
-            }
-            else if (sibs >= 0)
-            {
-                Console.Write("You will have a Vacation Home in Rural Pennslyvania,\n");
+                case 1:
+                    Console.WriteLine("You will have a Vacation Home in Argentina ");
+                    break;
+                case 2:
+                    Console.WriteLine("You will have a VAcation Home in Paris ");
+                    break;
+                case 3:
+                    Console.WriteLine("You will have a Vacation Home in Hawaii ");
+                    break;
+                case 4:
+                    Console.WriteLine("You will have a Vacation Home in Florida ");
+                    break;
+                case 5:
+                    Console.WriteLine("You will have a Vacation Home in Colorado ");
+                    break;
+                default:
+                    Console.WriteLine("You will have a Vacation Home in Pennslyvania ");
+                    break;
             }
             //still need to figue out how to streamline my code to less lines
             //need to work on switch statements
@@ -132,6 +133,7 @@ namespace FortuneTeller
                 Console.Write("You will have a Vehicle of your choice as Transportation. \n");
             }
             //end statement
+            
             Console.WriteLine(" \n Thank you for letting me predict your future!!");
 
 
